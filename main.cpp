96f8fd293f7cc1dcda5de3ff28e897abf4000052
Jesus Ramirez <A00835680@tec.mx>
2023-06-07 18:44:08 -0600

    c

--- main.cpp
+++ main.cpp
@@ -10,8 +10,7 @@ using namespace std;
 #include "CustomerParkingCard.hpp"
 #include "SupplierParkingCard.hpp"
 
-void readFromFile(ParkingCard *listOfCards[], int &numOfCards)
-{
+void readFromFile(ParkingCard *listOfCards[], int &numOfCards){
     ifstream inFile;
     inFile.open("C:/Users/jesus/Documents/GitHub/POO/POOproject/ParkingCards.txt");
     if (!inFile)
@@ -25,82 +24,154 @@ void readFromFile(ParkingCard *listOfCards[], int &numOfCards)
     string company;
     string store;
 
-    while(inFile >> type >> id >> name >> store)
+    while(inFile >> type)
     {
         if (type == "E")
         {
-            EmployeeParkingCard *epc = new EmployeeParkingCard(id, name, company);
-            listOfCards[id] = epc;
+            inFile >> id >> name >> store;
+            listOfCards[numOfCards] = new EmployeeParkingCard(id, name, store);
             numOfCards++;
         }
         else if (type == "C")
         {
-            CustomerParkingCard *cpc = new CustomerParkingCard(id, name);
-            listOfCards[id] = cpc;
+            inFile >> id >> name;
+            listOfCards[numOfCards] = new CustomerParkingCard(id, name);
             numOfCards++;
 
         }
         else if (type == "S")
         {
-            SupplierParkingCard *spc = new SupplierParkingCard(id, name, company);
-            listOfCards[id] = spc;
+            inFile >> id >> name >> store;
+            listOfCards[numOfCards] = new SupplierParkingCard(id, name, store);
             numOfCards++;
 
         }
     }
     inFile.close();
-
 }
 
 
 int main()
 {
+    ParkingRegister *listRegister[20];
+    int numOfRegisters = 0;
+
+    //Welcome and initializing today's rate
+    float todaysRate = 0.0;
+    cout<<"-----------------------------------"<<endl;
+    cout<<"Welcome to the Parking Lot"<<endl;
+    cout<<"-----------------------------------"<<endl;
+    cout<<"Please enter today's rate: ";
+    cin>>todaysRate;
+
     //Read from file
     ParkingCard *listOfCards[50];
     int numOfCards = 0;
     readFromFile(listOfCards, numOfCards);
-    for (int i = 0; i < numOfCards; i++)
+
+    //Menu
+    char option;
+
+    do
     {
-        listOfCards[i]->print();
-    }
+        cout<<"*****************************************"<<endl;
+        cout << "Options " << endl;
+        cout << endl;
+        cout <<"a) Register entrance to Parking Lot " << endl;
+        cout <<"b) Exit from the Parking Lot " << endl;
+        cout <<"c) Display Parking Lot information " << endl;
+        cout <<"d) Display Parking Cards information " << endl;
+        cout <<"e) Exit " << endl;
+        cout<<"*****************************************"<<endl;
+        cout << "option ->";
+        cin >> option;
+        switch (option) {
+            case 'a':
+            {
+                int checkID;
+                bool foundID=false;
+                bool checkHour=false;
+                bool checkMinutes=false;
+                do{
+                    cout<<"Enter ID: ";
+                    cin>>checkID;
+                    for (int i=0; i < numOfCards; i++)
+                    {
+                        if (listOfCards[i]->getCardID() == checkID)
+                        {
+                            foundID = true;
+                            break;
+                        }
+                    }
+                    if (foundID==false)
+                    {
+                        cout<<"Invalid ID"<<endl;
+                    }
+
+                }while (foundID==false);
+                
+                cout<<"Introduce time of entrance: "<<endl;
+                Time tEntrance;
+                int hour, minute;
+                do{
+                    cout<<"Enter Hour: ";
+                    cin>>hour;
+                    if (hour>=8 && hour<=22)
+                    {
+                        checkHour=true;
+                        tEntrance.setHour(hour);
+                    }
+                    else
+                    {
+                        cout<<"Invalid hour"<<endl;
+                    }
+
+                }while (checkHour==false);
+
+                do{
+                    cout<<"Enter Minutes: ";
+                    cin>>minute;
+                    if (minute>=0 && minute<=59)
+                    {
+                        checkMinutes=true;
+                        tEntrance.setMinute(minute);
+                    }
+                    else
+                    {
+                        cout<<"Invalid minutes"<<endl;
+                    }
+                }while (checkMinutes==false);
+                listRegister[numOfRegisters] = new ParkingRegister(checkID, tEntrance);
+                numOfRegisters++;
+                break;
+            }
+            case 'b':
+            {
+                cout<<"b"<<endl;
+                break;
+            }
+            case 'c':
+            {
+                cout<<"c"<<endl;
+                break;
+            }
+            case 'd':
+            {
+                cout<<"d"<<endl;
+                break;
+            }
+
+        }
+    } while (option != 'e');
 
 
 
-    int elapsed;
-    // Test Time class
-    Time t1(8, 46);
-    Time t2(15, 11);
-    elapsed = t1.elapsedTime(t2);
-    cout<<"Elapsed time: "<<elapsed<<endl;  
-    // Test ParkingRegister class
-    ParkingRegister pr(1234, t1);
-    pr.print();
+    //cout<<"numbers of cards: "<<numOfCards<<endl;
 
-    // Test Employee class
-    int hours;
-    double payment;
-    EmployeeParkingCard epc(1234, "John", "Walmart");
-    epc.print();
-    hours = epc.calculateHoursToPay(t1, t2);
-    cout<<"Hours to pay: "<<hours<<endl;
-    payment = epc.calculatePayment(t1, t2, 20);
-    cout<<"Payment: "<<payment<<endl;   
-
-    // Test Customer class
-    CustomerParkingCard cpc(1234, "Mary");
-    cpc.print();
-    hours = cpc.calculateHoursToPay(t2, t1);
-    cout<<"Hours to pay: "<<hours<<endl;
-    payment = cpc.calculatePayment(t2, t1, 20);
-    cout<<"Payment: "<<payment<<endl;
-
-    // Test Supplier class
-    SupplierParkingCard spc(1234, "John", "Walmart");
-    spc.print();
-    hours = spc.calculateHoursToPay(t1, t2);
-    cout<<"Hours to pay: "<<hours<<endl;
-    payment = spc.calculatePayment(t1, t2, 20);
-    cout<<"Payment: "<<payment<<endl;
+    //for (int i = 0; i < numOfCards; i++)
+    //{
+    //    listOfCards[i]->print();
+    //}
 
     return 0;
 
